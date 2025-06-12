3eff5193c45f7c2e12da99f1482c6f93ebab5fbd
sintaa02 <0110124056@student.nurulfikri.ac.id>
2025-06-12 13:15:54 +0700

    upload praktikum6

---
+++ praktikum06/views/detail.php
@@ -0,0 +1,29 @@
+<?php
+require_once("Controllers/Prodi.php")
+
+if (isset($_GET['id'])){
+    $id = $_GET['id'];
+    $data = $prodi->show($id);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detail Prodi</title>
+</head>
+<body>
+    <h1>Detail Prodi</h1>
+    <?php if($data)?>
+    <p>Kode: <?php = $data['kode'] ?></p>
+    <p>Nama: <?php = $data['nama'] ?></p>
+    <p>Kaprodi: <?php = $data['kaprodi'] ?></p>
+    <?php else: ?>b
+        <p>Data tidak ditemukan</p>
+    <?php endif; ?>
+    <a href="?url=prodi">Kembali</a>
+</body>
+</html>
